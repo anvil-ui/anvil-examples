63661000fd108eb199cb60276634a7b41f0fd12f
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2015-09-28 11:48:57 +0200

    ported anim-picker example to the new anvil

--- anim-picker/src/main/java/com/example/anvil/animpicker/AnimatedPickerView.java
+++ anim-picker/src/main/java/com/example/anvil/animpicker/AnimatedPickerView.java
@@ -1,18 +1,18 @@
 package com.example.anvil.animpicker;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import static trikita.anvil.v15.Attrs.*;
-import static trikita.anvil.v15.Anim.*;
 
-import trikita.anvil.State;
 import trikita.anvil.Anvil;
 import trikita.anvil.RenderableView;
-import android.widget.FrameLayout;
+import trikita.anvil.State;
 
 // A simple picker component that looks like this:
 //
@@ -52,8 +52,12 @@ public class AnimatedPickerView extends RenderableView {
 	// If both states are off - there is no animations and current item is
 	// shown in the center.
 	//
-	private State mNext = Anvil.newState(false);
-	private State mPrev = Anvil.newState(false);
+	enum Navigation {
+		NONE,
+		PREV,
+		NEXT
+	};
+	private State mState = State.init(Navigation.NONE);
 
 	private int mIndex = 0; // Current item index
 	private String[] mItems = { "Mercury", "Venus", "Earth", "Mars", "Jupiter",
@@ -73,97 +77,119 @@ public class AnimatedPickerView extends RenderableView {
 	// When next button is clicked - slowly change mNext state to true, then
 	// increase the item index, then set mNext back to false
 	private View.OnClickListener mOnNextClicked = (v) -> {
-		mNext.set(true, ANIM_DURATION, () -> {
+		mState.set(Navigation.NEXT, ANIM_DURATION, () -> {
 			mIndex = (mIndex + 1) % mItems.length;
-		}).set(false, 0);
+		}).set(Navigation.NONE, 0);
 	};
 
 	// When next button is clicked - slowly change mPrev state to true, then
 	// decrease the item index, then set mPrev back to false
 	private View.OnClickListener mOnPrevClicked = (v) -> {
-		mPrev.set(true, ANIM_DURATION, () -> {
+		mState.set(Navigation.PREV, ANIM_DURATION, () -> {
 			mIndex = (mIndex + mItems.length - 1) % mItems.length;
-		}).set(false, 0);
+		}).set(Navigation.NONE, 0);
 	};
 
-	public ViewNode view() {
-		return
-			v(LinearLayout.class,
-				size(FILL, WRAP),
-				orientation(LinearLayout.HORIZONTAL),
+	public void view() {
+		linearLayout(() -> {
+			size(FILL, WRAP);
+			orientation(LinearLayout.HORIZONTAL);
 
-				v(Button.class,
-					text("<<<"),
-					onClick(mOnPrevClicked)),
+			button(() -> {
+				text("<<<");
+				onClick(mOnPrevClicked);
+			});
 
-				// Container for two items at most (one is disappearing, another one
-				// replaces it). Centered item takes 50% of the width in the center,
-				// so it's left offset is 25% as well as its right offset
-				v(LinearLayout.class,
-					size(WRAP, FILL).weight(1),
-					// here we measure container width after it's inflated
-					config(v -> mWidth = v.getWidth()),
-					orientation(LinearLayout.HORIZONTAL),
+			// Container for two items at most (one is disappearing, another one
+			// replaces it). Centered item takes 50% of the width in the center,
+			// so it's left offset is 25% as well as its right offset
+			linearLayout(() -> {
+				size(WRAP, FILL);
+				weight(1);
+				orientation(LinearLayout.HORIZONTAL);
+				// here we measure container width after it's inflated (recurisvely
+				// calling render() until the views have finished their layout
+				if (mWidth <= 0) {
+					mWidth = Anvil.currentView().getWidth();
+					if (mWidth == 0) {
+						Anvil.currentView().post(() -> Anvil.render());
+					}
+				}
 
-					// Primary view, displays the current item
-					v(FrameLayout.class,
-						// takes 50% of the container
-						size(mWidth/2, FILL),
-						// if it's replaced by the previous item - it's shifted full-width
-						// to the left (from 25% to -25%) and fades out.
-						anim(mPrev.isOn())
-							.of("x", mWidth/4, -mWidth/4)
-							.of("alpha", 1f, 0f)
-							.duration(ANIM_DURATION),
-						// if it's replaced by the next item - it's shifted full-width
-						// to the right (from 25% to 75%) and fades out.
-						anim(mNext.isOn())
-							.of("x", mWidth/4, mWidth*3/4)
-							.of("alpha", 1f, 0f)
-							.duration(ANIM_DURATION),
-						// if it's not animated at all - it's centered and is fully opaque
-						mNext.isOff() && mPrev.isOff() ? x(mWidth/4) : null,
-						mNext.isOff() && mPrev.isOff() ? alpha(1f) : null,
+				System.out.println("ANIM NEXT="+mState.get());
 
-						// see below, custom method to render item content
-						itemView(mIndex)),
+				// Primary view, displays the current item
+				frameLayout(() -> {
+					// takes 50% of the container
+					size(mWidth/2, FILL);
+					// if it's replaced by the previous item - it's shifted full-width
+					// to the left (from 25% to -25%) and fades out.
+					anim(mState.is(Navigation.PREV),
+						of("x", mWidth/4, -mWidth/4)
+						.of("alpha", 1f, 0f)
+						.duration(ANIM_DURATION));
+					// if it's replaced by the next item - it's shifted full-width
+					// to the right (from 25% to 75%) and fades out.
+					anim(mState.is(Navigation.NEXT),
+						of("x", mWidth/4, mWidth*3/4)
+						.of("alpha", 1f, 0f)
+						.duration(ANIM_DURATION));
+					// if it's not animd at all - it's centered and is fully opaque
+					// see below, custom method to render item content
+					if (mState.is(Navigation.NONE)) {
+						System.out.println("back to default values");
+						x(mWidth/4);
+						alpha(1f);
+					}
 
-					// Helper view - the "next" or the "previous" item
-					// It is shown only during the animation phase
-					v(FrameLayout.class,
-						size(mWidth/2, FILL),
-						// previous item is moving from 75% to 25% of the container width
-						anim(mPrev.isOn())
-							.of("x", mWidth*3/4, mWidth/4)
-							.of("alpha", 0f, 1f)
-							.duration(ANIM_DURATION),
-						// next item is moving from -25% to 25% of the container width
-						anim(mNext.isOn())
-							.of("x", -mWidth/4, mWidth/4)
-							.of("alpha", 0f, 1f)
-							.duration(ANIM_DURATION),
-						// if no animation happens now - hide this helper view
-						visibility(mNext.isOn() || mPrev.isOn()),
+					itemView(mIndex);
+				});
 
-						// display the contents of the next or previous item in the list
-						itemView(mIndex + (mNext.isOn() ? 1 : -1)))),
 
-				v(Button.class,
-						text(">>>"),
-						onClick(mOnNextClicked)));
+				// Helper view - the "next" or the "previous" item
+				// It is shown only during the animation phase
+				frameLayout(() -> {
+					size(mWidth/2, FILL);
+					// previous item is moving from 75% to 25% of the container width
+					anim(mState.is(Navigation.PREV),
+						of("x", mWidth*3/4, mWidth/4)
+						.of("alpha", 0f, 1f)
+						.duration(ANIM_DURATION));
+					// next item is moving from -25% to 25% of the container width
+					anim(mState.is(Navigation.NEXT),
+						of("x", -mWidth/4, mWidth/4)
+						.of("alpha", 0f, 1f)
+						.duration(ANIM_DURATION));
+					// if no animation happens now - hide this helper view
+					visibility(!mState.is(Navigation.NONE));
+					// display the contents of the next or previous item in the list
+					if (mState.is(Navigation.NEXT)) {
+						itemView(mIndex + 1);
+					} else {
+						itemView(mIndex - 1);
+					}
+				});
+			});
+
+			button(() -> {
+				text(">>>");
+				onClick(mOnNextClicked);
+			});
+		});
 	}
 
 	//
 	// Let's separate rendering of the individual item from the generic picker
 	// component to make customizations easier
 	//
-	public ViewNode itemView(int index) {
+	public void itemView(int index) {
 		// Get text of the item in the list (using modulo, non-negative index)
 		String item = mItems[(index + mItems.length) % mItems.length];
-		return
-			v(TextView.class,
-				size(FILL, FILL),
-				gravity(CENTER),
-				text(item));
+
+		textView(() -> {
+			size(FILL, FILL);
+			gravity(CENTER);
+			text(item);
+		});
 	}
 }
